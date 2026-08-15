f88cb6b997b0fb3c0801d4852dbbece85a2fd0c8
Luis Lecaros <luis.lecaros.d@gmail.com>
2026-08-15 16:24:21 -0500

    fix: sincronizar build10021 -> build10022 en asistencia, justificaciones, esbas-24 y asistencia/app.js

--- asistencia/app.js
+++ asistencia/app.js
@@ -450,7 +450,7 @@ function generarMensajeWhatsApp() {
     
     const appVersion = (typeof window.obtenerEtiquetaVersionAsistia === "function") 
         ? window.obtenerEtiquetaVersionAsistia("short") 
-        : "v1.1.0-build10021"
+        : "v1.1.0-build10022"
     
     const msg = `ID reporte:
 ${idReporte}
